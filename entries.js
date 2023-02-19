de151b48ff3049c9fc9093cfce6b8bbdf042c8cd
Mikkel Svartveit <mikkel.svartveit@gmail.com>
2023-02-19 21:09:24 +0100

    Add index.html to allow redirecting root path

--- entries.js
+++ entries.js
@@ -1,6 +1,6 @@
 export default {
   // Define routes here:
-  "": "mikkelsvartveit.com",
+  "": "https://mikkelsvartveit.com",
   bb: "https://ntnu.blackboard.com/ultra/course",
   dns: "https://domene.shop/admin?id=1887412&edit=dns&advanced=1",
 };
