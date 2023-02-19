4dbcca9efefa6ff6149e0012089636dc7058ad68
Mikkel Svartveit <mikkel.svartveit@gmail.com>
2023-02-19 23:12:33 +0100

    Add entry 'vm'

--- entries.js
+++ entries.js
@@ -9,4 +9,5 @@ export default {
   yt: "https://www.youtube.com/channel/UCiJ7FdLKJSpGs1pqjqd7VnA",
   xcom: "https://calendar.google.com/calendar/hosted/xcomdata.no",
   tp: "https://ntnu.1024.no/mikkesva",
+  vm: "https://ssh.cloud.google.com/v2/ssh/projects/electric-abode-375721/zones/us-east1-b/instances/vm-free-tier?authuser=0&hl=en_US&projectNumber=249661450500&useAdminProxy=true",
 };
