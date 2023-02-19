c82d0adec53d2c7750bbe04d972fb345a4d71c5a
Mikkel Svartveit <mikkel.svartveit@gmail.com>
2023-02-19 21:19:05 +0100

    Add route 'wca'

--- entries.js
+++ entries.js
@@ -3,4 +3,5 @@ export default {
   "": "https://mikkelsvartveit.com",
   bb: "https://ntnu.blackboard.com/ultra/course",
   dns: "https://domene.shop/admin?id=1887412&edit=dns&advanced=1",
+  wca: "https://www.worldcubeassociation.org/persons/2014SVAR02",
 };
