0e3dacaf8f20d389319072d2c3b812c88d76a22b
Mikkel Svartveit <30391413+mikkelsvartveit@users.noreply.github.com>
2023-03-04 14:22:41 +0100

    Update entries.js

--- entries.js
+++ entries.js
@@ -11,4 +11,5 @@ export default {
   xcom: "https://calendar.google.com/calendar/hosted/xcomdata.no",
   tp: "https://ntnu.1024.no/mikkesva",
   vm: "https://ssh.cloud.google.com/v2/ssh/projects/electric-abode-375721/zones/us-east1-b/instances/vm-free-tier?authuser=0&hl=en_US&projectNumber=249661450500&useAdminProxy=true",
+  valg: "https://docs.google.com/forms/d/1IuRyYjLDjOdQzR-WJBAeG6Wg0Oh3bYZnq1WpSbRpgXg/edit",
 };
