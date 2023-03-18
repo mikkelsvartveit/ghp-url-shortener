e668a705018a038be510df65a7944ae2e5e3a447
Mikkel Svartveit <30391413+mikkelsvartveit@users.noreply.github.com>
2023-03-18 16:45:07 +0100

    Update entries.js

--- entries.js
+++ entries.js
@@ -5,7 +5,7 @@ export default {
   bb: "https://ntnu.blackboard.com/ultra/course",
   dns: "https://domene.shop/admin?id=1887412&edit=dns&advanced=1",
   wca: "https://www.worldcubeassociation.org/persons/2014SVAR02",
-  f1: "https://live1.formula1stream.cc",
+  f1: "https://www.latifistreams.com/motorsport",
   gh: "https://github.com/mikkelsvartveit",
   yt: "https://www.youtube.com/channel/UCiJ7FdLKJSpGs1pqjqd7VnA",
   xcom: "https://calendar.google.com/calendar/hosted/xcomdata.no",
