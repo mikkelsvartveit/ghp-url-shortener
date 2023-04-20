91cf1d2474c59d05eacc5304e094e3df71bb427b
Mikkel Svartveit <30391413+mikkelsvartveit@users.noreply.github.com>
2023-04-20 10:40:36 +0200

    Update entries.js

--- entries.js
+++ entries.js
@@ -1,20 +1,5 @@
 export default {
   // Define routes here:
   "": "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-  add: "https://github.com/mikkelsvartveit/ghp-url-shortener/edit/master/entries.js",
-  bb: "https://ntnu.blackboard.com/ultra/course",
-  dns: "https://dash.cloudflare.com/0356594c55b7033aa58b3e893df1e9bb/misva.no/dns/records",
-  wca: "https://www.worldcubeassociation.org/persons/2014SVAR02",
-  f1: "https://www.latifistreams.com/motorsport",
-  gh: "https://github.com/mikkelsvartveit",
-  yt: "https://www.youtube.com/channel/UCiJ7FdLKJSpGs1pqjqd7VnA",
-  xcom: "https://calendar.google.com/calendar/hosted/xcomdata.no",
-  tp: "https://ntnu.1024.no/mikkesva",
-  vm: "https://ssh.cloud.google.com/v2/ssh/projects/electric-abode-375721/zones/us-east1-b/instances/vm-free-tier?authuser=0&hl=en_US&projectNumber=249661450500&useAdminProxy=true",
-  dotfiles: "https://github.com/mikkelsvartveit/dotfiles",
-  vim: "https://github.com/mikkelsvartveit/dotfiles/blob/main/vim/.vimrc",
-  neovim: "https://github.com/mikkelsvartveit/dotfiles/tree/main/nvim",
-  rpifish: "https://gist.github.com/Brandonshire/73098f982d00d13ddf93a8d484160da2?permalink_comment_id=4470567#gistcomment-4470567",
-  lens: "https://camerasize.com/compact/#703.408,703.596,703,ha,t",
-  vimrc: "https://raw.githubusercontent.com/mikkelsvartveit/dotfiles/main/vim/.vimrc",
+  customroute: "https://google.com",
 };
